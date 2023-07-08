dc35694228a95ff8e2b554ccd615ecb05a37ab10
darshanvjani <35656144+darshanvjani@users.noreply.github.com>
2023-07-07 19:42:00 -0700

    training model script

---
+++ Introduction To Pytorch/train.py
@@ -0,0 +1,139 @@
+import model
+from utils import check_cuda
+from tqdm import tqdm
+import torch
+import matplotlib.pyplot as plt
+
+
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+has_trained = False
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, criterion):
+  
+  global train_acc
+  global train_losses
+  global test_acc
+  global test_losses
+
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader, criterion):
+    global train_acc
+    global train_losses
+    global test_acc
+    global test_losses
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def training(model, train_loader, test_loader, optimizer, criterion, no_of_epochs, scheduler):
+  
+  global has_trained
+  has_trained = True
+  hardware = "cuda" if check_cuda() else "cpu"
+  device = torch.device(hardware)
+
+  for epoch in range(1, no_of_epochs+1):
+    print(f'Epoch {epoch}')
+    train(model, device, train_loader, optimizer, criterion)
+    test(model, device, test_loader, criterion)
+    scheduler.step()
+
+def training_stats():
+  if has_trained == False:
+    print("Model Not Yet Trained")
+    return 0
+  
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
